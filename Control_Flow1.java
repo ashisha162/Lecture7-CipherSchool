0da69e7b64d3ef65f1ebd76351721120bb6ddae0
Ashish Kumar <104408789+ashisha162@users.noreply.github.com>
2023-06-17 10:23:14 +0530

    Update Control_Flow1.java

--- Control_Flow1.java
+++ Control_Flow1.java
@@ -1,1 +1,38 @@
 
+import java.util.*;
+
+
+public class Control_Flow1
+{
+	int findMax(int a,int b,int c)
+{
+	if(a>b)//nested if else//
+	{
+		if(a>c)
+		{
+			return a;
+		}
+		else
+		{
+			return c;
+		}
+	}
+	else
+	{
+		if(b>c)
+		{
+			return b;
+		}
+		else
+		{
+			return c;
+		}
+	}
+	
+}
+public static void main(String args[])
+{
+Control_Flow1 cf=new Control_Flow1();
+System.out.println(cf.findMax(8,10,12));
+}
+}
